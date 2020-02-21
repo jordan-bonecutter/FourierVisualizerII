343b27951b41d755c0daddfa3bf5a8df430853b4
Jordan Bonecutter <jordanbonecutter@gmail.com>
2020-02-20 16:18:34 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,130 @@
+#
+#
+#
+#
+
+import cairo
+import numpy as np
+import cv2 as cv
+import svgpathparser
+from math import pi as M_PI
+from math import e as M_E
+
+# target output video size and length
+OUT_WIDTH = 1920//3
+OUT_HEIGHT = 1080//3
+OUT_FPS = 60
+FRAME_COUNT = 10*OUT_FPS
+
+
+def save_frame(tosave, vwriter, tbuff=None):
+  h, w, c = tosave.shape
+  if tbuff is None:
+    tbuff = np.zeros((h, w, 3), dtype=np.uint8)
+  for y in range(tbuff.shape[0]):
+    for x in range(tbuff.shape[1]):
+      tbuff[h - y - 1, x] = (tosave[y, x][0], tosave[y, x][1], tosave[y, x][2])
+  vwriter.write(tbuff)
+  return tbuff
+
+
+def get_freq(index, n_freqs):
+  scaled = 2.*(index/n_freqs)
+  if int(scaled)%2 == 1:
+    return ((scaled*M_PI)%M_PI) - M_PI
+  else:
+    return (scaled*M_PI)%M_PI
+
+
+def get_cairo_point(inpoint, offset, scale):
+  global OUT_WIDTH, OUT_HEIGHT
+  return ((inpoint[0]-offset[0])/scale + OUT_WIDTH/2, OUT_HEIGHT/2 - (inpoint[1]-offset[1])/scale)
+
+
+def main(argv) -> int:
+  global OUT_HEIGHT, OUT_WIDTH
+  if len(argv) != 3:
+    print('Invalid usage: ' + argv[0] + ' <input.svg> <output.avi>')
+    return 1
+  # extract points from svg
+  with open(argv[1], 'r') as fi:
+    path = svgpathparser.firstpathinfile(fi)
+  if not path:
+    print('No path in svg file')
+    return 0
+  points = svgpathparser.readpath(path.attributes['d']).getpoints()
+
+  # get scaling informations
+  bounds = max(points, key=lambda x:x.real).real, max(points, key=lambda x:x.imag).imag, \
+           min(points, key=lambda x:x.real).real, min(points, key=lambda x:x.imag).imag
+  pwidth = bounds[0] - bounds[2]
+  pheight = bounds[1] - bounds[3]
+  wscale = pwidth/(OUT_WIDTH)
+  hscale = pheight/(OUT_HEIGHT)
+  scale = max(wscale, hscale)/0.9
+  xoff = (bounds[0] + bounds[2])/2
+  yoff = (bounds[1] + bounds[3])/2
+  off  = (xoff, yoff)
+
+  # take the dft and sort it so that the
+  # lowest freq are at the bottom
+  freqs  = np.fft.fft(points)  
+  sfs    = np.fft.fftfreq(len(points))
+  freqs  = [(freqs[i], sfs[i]) for i in range(len(freqs))]
+  freqs  = sorted(freqs, key=lambda x: abs(x[1]))
+
+  # create current image buffer
+  image  = np.zeros((OUT_HEIGHT, OUT_WIDTH, 4), dtype=np.uint8)
+  tbuff  = None
+
+  # setup cairo
+  sfc    = cairo.ImageSurface.create_for_data(image, cairo.FORMAT_ARGB32, OUT_WIDTH, OUT_HEIGHT)
+  ctx    = cairo.Context(sfc)
+  ctx.set_line_width(2)
+  ctx.set_line_cap(cairo.LineCap.ROUND)
+  vwriter = cv.VideoWriter(argv[1],cv.VideoWriter_fourcc(*'MJPG'),OUT_FPS,(OUT_WIDTH, OUT_HEIGHT))
+
+  # draw the frames
+  todraw = [points[0]]
+  zeropoint = get_cairo_point((todraw[0].real, todraw[0].imag), off, scale)
+  framessaved = 0
+  for t in range(FRAME_COUNT):
+    theta = (t / (FRAME_COUNT-1))*len(points)*2*M_PI
+    curr = 0
+
+    ctx.set_source_rgb(1, 1, 1)
+    ctx.rectangle(0, 0, OUT_WIDTH, OUT_HEIGHT)
+    ctx.fill()
+    ctx.set_source_rgb(0.7, 0.7, 0.7)
+
+    n = 0
+    for weight, freq in freqs:
+      center = get_cairo_point((curr.real, curr.imag), off, scale)
+      curr += weight*pow(M_E, 1j*freq*theta)/len(points)
+      if n != 0:
+        ctx.set_line_width(2)
+        ctx.arc(center[0], center[1], abs(weight)/(scale*len(points)), 0, 2*M_PI)
+        ctx.stroke()
+        ctx.move_to(center[0], center[1])
+        currpoint = get_cairo_point((curr.real, curr.imag), off, scale) 
+        ctx.line_to(currpoint[0], currpoint[1])
+        ctx.stroke()
+      n += 1
+
+    todraw.append(curr)
+    ctx.move_to(zeropoint[0], zeropoint[1])
+    ctx.set_source_rgb(1, 0.05, 0.05)
+    for i in range(1, len(todraw)):
+      draw = todraw[i]
+      cdraw = get_cairo_point((draw.real, draw.imag), off, scale)
+      ctx.line_to(cdraw[0], cdraw[1])
+    ctx.stroke()
+    tbuff = save_frame(image, vwriter, tbuff)
+    print(framessaved)
+    framessaved += 1
+  vwriter.release()
+
+if __name__ == '__main__':
+  import sys
+  main(sys.argv)
+
---
+++ svgpathparser.py
@@ -0,0 +1,525 @@
+#
+#
+#
+#
+
+import numpy as np
+import cv2 as cv
+import pyxml
+
+teststr = "M1084.5,1622.5c195-93,405-249,472-382s-75-254-121-16-106,496-106,496,44-231,126-262,164-28,149,61-48,208,63,208,248-113,249-199-118.5-149.5-144-14c-26.13,138.85,21,210,163,202s297-315,313-474-190-215-188,109,79,363,168,363,306-194,325-461-176-149-189,78,42,367,164,365,83-249,279-260,97,294-25,291S2603,1599,2671,1516s221.5-58.5,280.5-77.5,105-60,105-60"
+
+def bezier(t, z0, z1):
+  return z0*(1-t) + z1*t
+
+def qbez(t, z0, z1, z2):
+  return z1 + ((1-t)*(1-t))*(z0 - z1) + t*t*(z2 - z1)
+
+def cbez(t, z0, z1, z2, z3):
+  return ((1-t)*(1-t)*(1-t))*z0 + 3*((1-t)*(1-t)*t)*z1 + 3*((1-t)*t*t)*z2 + t*t*t*z3
+
+def n_bezier(t, *control_points):
+  if len(control_points) == 2:
+    return bezier(t, control_points[0], control_points[1])
+  return bezier(t, n_bezier(t, *control_points[:-1]), n_bezier(t, *control_points[1:]))
+
+def _findfirstpath(root):
+  # dfs for first path in file
+  if len(root.children) == 0:
+    return None
+
+  for child in root.children:
+    if child.name == 'path':
+      return child
+    ret =  _findfirstpath(child)
+    if ret != None:
+      return ret
+
+  return None
+  
+def firstpathinfile(fd):
+  # read contents
+  contents = pyxml.xml_tree.fromFile(fd)
+  return _findfirstpath(contents.root)
+
+def readpath(pathstr):
+  ret = SVGPathArtist()
+  r_i = 0
+  currcommand = None
+  while r_i < len(pathstr):
+    # read in whitespace
+    while pathstr[r_i].isspace():
+      r_i += 1
+      if r_i == len(pathstr):
+        return ret
+
+    # new command character
+    if pathstr[r_i].isalpha():
+      currcommand = pathstr[r_i]
+      r_i += 1
+
+    while r_i < len(pathstr) and pathstr[r_i].isspace():
+      r_i += 1
+
+    if currcommand.upper() == 'M':
+      nx = ''
+      ny = ''
+      b  = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        nx += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        ny += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      if currcommand.islower():
+        ret.moveto(complex(float(nx), float(ny)), absolute=False)
+      else:
+        ret.moveto(complex(float(nx), float(ny)), absolute=True)
+
+    elif currcommand.upper() == 'Z':
+      ret.closepath()
+
+    elif currcommand.upper() == 'L':
+      lx = ''
+      ly = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        lx += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        ly += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      if currcommand.islower():
+        ret.lineto(complex(float(lx), float(ly)), absolute=False)
+      else:
+        ret.lineto(complex(float(lx), float(ly)), absolute=True)
+      
+    elif currcommand.upper() == 'H':
+      nx = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        nx += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      if currcommand.islower():
+        ret.horizontalto(float(nx), absolute=False)
+      else:
+        ret.horizontalto(float(nx), absolute=True)
+
+    elif currcommand.upper() == 'V':
+      ny = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] !='-'or r_i == b):
+        ny += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      if currcommand.islower():
+        ret.verticalto(float(ny), absolute=False)
+      else:
+        ret.verticalto(float(ny), absolute=True)
+
+    elif currcommand.upper() == 'C':
+      c1x = ''
+      c1y = ''
+      c2x = ''
+      c2y = ''
+      nx  = ''
+      ny  = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c1x += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c1y += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c2x += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c2y += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        nx += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        ny += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      cp1 = complex(float(c1x), float(c1y))
+      cp2 = complex(float(c2x), float(c2y))
+      np  = complex(float(nx),  float(ny))
+
+      if currcommand.islower():
+        ret.curveto(cp1, cp2, np, absolute=False)
+      else:
+        ret.curveto(cp1, cp2, np, absolute=True)
+
+    elif currcommand.upper() == 'S':
+      c2x = ''
+      c2y = ''
+      nx  = ''
+      ny  = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c2x += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c2y += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        nx += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        ny += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      cp2 = complex(float(c2x), float(c2y))
+      np  = complex(float(nx),  float(ny))
+
+      if currcommand.islower():
+        ret.smoothcurveto(cp2, np, absolute=False)
+      else:
+        ret.smoothcurveto(cp2, np, absolute=True)
+
+    elif currcommand.upper() == 'Q':
+      c1x = ''
+      c1y = ''
+      nx  = ''
+      ny  = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c1x += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        c1y += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        nx += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        ny += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      cp1 = complex(float(c2x), float(c2y))
+      np  = complex(float(nx),  float(ny))
+
+      if currcommand.islower():
+        ret.qcurveto(cp1, np, absolute=False)
+      else:
+        ret.qcurveto(cp1, np, absolute=True)
+
+    elif currcommand.upper() == 'T':
+      nx  = ''
+      ny  = ''
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (pathstr[r_i] != '-' or b == r_i) and pathstr[r_i] != ',':
+        nx += pathstr[r_i]
+        r_i += 1
+
+      if pathstr[r_i] == ',':
+        r_i += 1
+
+      # read in whitespace
+      while pathstr[r_i].isspace():
+        r_i += 1
+      
+      b = r_i
+      while (not pathstr[r_i].isalpha()) and pathstr[r_i] != ',' and (pathstr[r_i] != '-'or b==r_i):
+        ny += pathstr[r_i]
+        r_i += 1
+        if r_i == len(pathstr):
+          break
+
+      if r_i < len(pathstr) and pathstr[r_i] == ',':
+        r_i += 1
+
+      np  = complex(float(nx),  float(ny))
+
+      if currcommand.islower():
+        ret.smoothqcurveto(np, absolute=False)
+      else:
+        ret.smoothqcurveto(np, absolute=True)
+
+  return ret
+
+
+class SVGPathArtist:
+  def __init__(self):
+    self.cursor = complex(0)
+    self.instr  = []
+
+  def moveto(self, point, absolute=True):
+    if absolute:
+      self.cursor = point 
+    else:
+      self.cursor += point
+
+  def closepath(self):
+    self.instr.append(('z'))      
+
+  def lineto(self, point, absolute=True):
+    if absolute:
+      self.instr.append(('l', self.cursor, point))
+      self.cursor = point
+    else:
+      self.instr.append(('l', self.cursor, self.cursor + point))
+      self.cursor += point
+
+  def horizontalto(self, h, absolute=True):
+    if absolute:
+      self.instr.append(('l', self.cursor, complex(h, self.cursor.imag)))
+      self.cursor = complex(h, self.cursor.imag)
+    else:
+      self.instr.append(('l', self.cursor, complex(self.cursor.real + h, self.cursor.imag)))
+      self.cursor = complex(self.cursor.real + h, self.cursor.imag)
+
+  def verticalto(self, v, absolute=True):
+    if absolute:
+      self.instr.append(('l', self.cursor, complex(self.cursor.real, v)))
+      self.cursor = complex(self.cursor.real, v)
+    else:
+      self.instr.append(('l', self.cursor, complex(self.cursor.real, self.cursor.imag + v)))
+      self.cursor = complex(self.cursor.real, self.cursor.imag + v)
+
+  def curveto(self, cp1, cp2, cp3, absolute=True):
+    if absolute:
+      self.instr.append(('c', self.cursor, cp1, cp2, cp3))
+      self.cursor = cp3
+    else:
+      self.instr.append(('c', self.cursor, self.cursor+cp1, self.cursor+cp2, self.cursor+cp3))
+      self.cursor += cp3
+
+  def smoothcurveto(self, cp2, cp3, absolute=True):
+    if absolute:
+      previ = self.instr[-1]
+      cp1 = previ[3] + 2*(previ[4] - previ[3])
+      self.instr.append(('c', self.cursor, cp1, cp2, cp3))
+      self.cursor = cp3
+    else:
+      previ = self.instr[-1]
+      cp1 = previ[3] + 2*(previ[4] - previ[3])
+      self.instr.append(('c', self.cursor, cp1, self.cursor+cp2, self.cursor+cp3))
+      self.cursor += cp3
+
+  def qcurveto(self, cp1, cp2, absolute=True):
+    if absolute:
+      self.instr.append(('q', self.cursor, cp1, cp2))
+      self.cursor = cp2
+    else:
+      self.instr.append(('q', self.cursor, self.cursor+cp1, self.cursor+cp2))
+      self.cursor += cp2
+
+  def smoothqcurveto(self, cp2, absolute=True):
+    if absolute:
+      previ = self.instr[-1]
+      cp1   = previ[1] + 2*(previ[2] - previ[1])
+      self.instr.append(('q', self.cursor, cp1, cp2))
+      self.cursor = cp2
+    else:
+      previ = self.instr[-1]
+      cp1   = previ[1] + 2*(previ[2] - previ[1])
+      self.instr.append(('q', self.cursor, cp1, self.cursor+cp2))
+      self.cursor += cp2
+
+  def getpoints(self, translate=complex(0)):
+    points = []
+    for instr in self.instr:
+      if instr[0] == 'l':
+        for t in range(100):
+          points.append(bezier(t/100., instr[1], instr[2]) + translate)
+      elif instr[0] == 'q':
+        for t in range(100):
+          points.append(n_bezer(t/100., instr[1], instr[2], instr[3]) + translate)
+      elif instr[0] == 'c':
+        for t in range(100):
+          points.append(n_bezier(t/100., instr[1], instr[2], instr[3], instr[4]) + translate)
+    return points
